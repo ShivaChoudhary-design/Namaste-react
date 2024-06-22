366c9fa3423d619e73fa32ca7a6da33d02569818
ShivaChoudhary-design <shivachaudhary609@gmail.com>
2024-06-22 18:22:18 +0530

    added: mock data to fix swiggy api issue

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -3,9 +3,14 @@ import useRestaurantMenu from "../utils/useRestaurantMenu";
 import RestaurantCategory from "./RestaurantCategory";
 import { useState } from "react";
 import MenuShimmer from "./MenuShimmer";
+import {
+  RestaurantCuisineCost,
+  RestauratNames,
+} from "./mocks/restuarant-menu-mock";
+import resInfo from "./mocks/res-menu.json";
 const RestaurantMenu = () => {
   const { resId } = useParams();
-  const resInfo = useRestaurantMenu(resId);
+  // const resInfo = useRestaurantMenu(resId);
   const [showIndex, setShowIndex] = useState(null);
 
   const handleShowIndex = (index) => {
@@ -15,52 +20,53 @@ const RestaurantMenu = () => {
   if (resInfo === null) {
     return <MenuShimmer />;
   }
+  // added ?.data
   const { name, cuisines, costForTwoMessage, city, avgRating } =
-    resInfo?.cards[0]?.card?.card?.info ||
-    resInfo?.cards[1]?.card?.card?.info ||
-    resInfo?.cards[2]?.card?.card?.info;
+    resInfo?.data?.cards[0]?.card?.card?.info ||
+    resInfo?.data?.cards[1]?.card?.card?.info ||
+    resInfo?.data?.cards[2]?.card?.card?.info;
 
   const categories =
-    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
+    resInfo?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
       (c) =>
         c?.card?.card?.["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
     ) ||
-    resInfo?.cards[3]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
+    resInfo?.data?.cards[3]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
       (c) =>
         c?.card?.card?.["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
     ) ||
-    resInfo?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
+    resInfo?.data?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
       (c) =>
         c?.card?.card?.["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
     ) ||
-    resInfo?.cards[1]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
+    resInfo?.data?.cards[1]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
       (c) =>
         c?.card?.card?.["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
     ) ||
-    resInfo?.cards[0]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
+    resInfo?.data?.cards[0]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
       (c) =>
         c?.card?.card?.["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
     ) ||
-    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
+    resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
       (c) =>
         c?.card?.card?.["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
     );
 
-  
-
-  
-
   return (
     <div className="text-center">
-      <h1 className="font-bold  my-4 md:my-6 text-xl md:text-2xl">{name}</h1>
+      <h1 className="font-bold  my-4 md:my-6 text-xl md:text-2xl">
+        {/* {name} */}
+        {RestauratNames[resId]}
+      </h1>
       <p className="font-bold text-base md:text-lg">
-        {cuisines.join(", ")} - {costForTwoMessage}
+        {/* {cuisines.join(", ")} - {costForTwoMessage} */}
+        {RestaurantCuisineCost[resId]}
       </p>
       {/* Categories accordians */}
       {categories?.map((category, index) => (
